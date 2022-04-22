c8717785ab16b97ab9074596fb623df56bc0e602
xeladu <alex.duschek@googlemail.com>
2022-04-22 20:46:43 +0200

    Enhance demo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +9,290 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  BitmapDescriptor? _customIcon;
+  GoogleMapController? _controller;
+  Location? _location;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+
+    _initLocationService();
+  }
+
+  // check if service enabled
+  // ask to enable service
+  // check if permission granted
+  // ask for permission
+  Future _initLocationService() async {
+    _location = Location();
+
+    if (!await _location!.serviceEnabled()) {
+      if (!await _location!.requestService()) {
+        return;
+      }
+    }
+
+    var permission = await _location!.hasPermission();
+    if (permission == PermissionStatus.denied) {
+      permission = await _location!.requestPermission();
+      if (permission != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    var loc = await _location!.getLocation();
+    print("${loc.latitude} ${loc.longitude}");
+  }
+
+  Future _setCustomMarkerImage() async {
+    if (_customIcon != null) return;
+
+    // replaces the default marker with an asset image "icon.png"
+    final ImageConfiguration imageConfiguration =
+        createLocalImageConfiguration(context, size: const Size.square(24));
+    var bmp = await BitmapDescriptor.fromAssetImage(
+        imageConfiguration, "icon.png",
+        mipmaps: false);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _customIcon = bmp;
     });
+  }
+
+  Future _moveToBerlin() async {
+    _controller!.moveCamera(CameraUpdate.newLatLng(
+        const LatLng(52.52309894124325, 13.413122125924026)));
+  }
+
+  Future _animateToBerlin() async {
+    _controller!.animateCamera(CameraUpdate.newLatLng(
+        const LatLng(52.52309894124325, 13.413122125924026)));
+  }
+
+  Future _moveToCurrentLocation() async {
+    var loc = await _location!.getLocation();
+
+    if (loc.latitude == null || loc.longitude == null) return;
+
+    _controller!.moveCamera(
+        CameraUpdate.newLatLng(LatLng(loc.latitude!, loc.longitude!)));
+  }
+
+  Future _animateToCurrentLocation() async {
+    var loc = await _location!.getLocation();
+
+    if (loc.latitude == null || loc.longitude == null) return;
+
+    _controller!.animateCamera(
+        CameraUpdate.newLatLng(LatLng(loc.latitude!, loc.longitude!)));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    _setCustomMarkerImage();
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        bottomNavigationBar: BottomAppBar(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+              IconButton(
+                  icon: const Icon(
+                    Icons.forward,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () async => await _animateToBerlin()),
+              IconButton(
+                  icon: const Icon(
+                    Icons.pin_drop,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () async => await _animateToCurrentLocation()),
+              IconButton(
+                  icon: const Icon(Icons.fast_forward),
+                  color: Colors.blue,
+                  onPressed: () async => await _moveToBerlin()),
+            ])),
+        body: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: GoogleMap(
+                onMapCreated: (controller) async {
+                  _controller = controller;
+                  await _moveToCurrentLocation();
+                },
+                onTap: (latlng) =>
+                    print("onTap: ${latlng.latitude} ${latlng.longitude}"),
+                onLongPress: (latlng) => print(
+                    "onLongPress: ${latlng.latitude} ${latlng.longitude}"),
+                onCameraMove: (cp) => print(
+                    "onCameraMove: ${cp.target.latitude} ${cp.target.longitude}"),
+                compassEnabled: true,
+                myLocationButtonEnabled: true,
+                rotateGesturesEnabled: true,
+                zoomControlsEnabled: true,
+                zoomGesturesEnabled: true,
+                myLocationEnabled: true,
+                markers: <Marker>{
+                  Marker(
+                      markerId: const MarkerId("BerlinMarker"),
+                      rotation: 180,
+                      consumeTapEvents: true,
+                      onTap: () => print(
+                          "infoWindow is ignored, if consumeTapEvents is true!"),
+                      infoWindow: const InfoWindow(
+                          title: "Berlin",
+                          snippet:
+                              "Location is 52.52309894124325, 13.413122125924026"),
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueAzure),
+                      position:
+                          const LatLng(52.52309894124325, 13.413122125924026)),
+                  Marker(
+                      markerId: const MarkerId("StuttgartMarker"),
+                      rotation: 270,
+                      infoWindow: const InfoWindow(
+                          title: "Stuttgart",
+                          snippet:
+                              "Location is 48.77538798756846, 9.18178996130423"),
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueGreen),
+                      position:
+                          const LatLng(48.77538798756846, 9.18178996130423)),
+                  Marker(
+                      markerId: const MarkerId("FrankfurtMarker"),
+                      rotation: 0,
+                      infoWindow: const InfoWindow(
+                          title: "Frankfurt",
+                          snippet:
+                              "Location is 50.11655301417056, 8.681184968800679"),
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueViolet),
+                      position:
+                          const LatLng(50.11655301417056, 8.681184968800679)),
+                  Marker(
+                      markerId: const MarkerId("DüsseldorfMarker"),
+                      rotation: 90,
+                      infoWindow: const InfoWindow(
+                          title: "Düsseldorf",
+                          snippet:
+                              "Location is 51.22837532659796, 6.78887549590447"),
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueRose),
+                      position:
+                          const LatLng(51.22837532659796, 6.78887549590447)),
+                  Marker(
+                      markerId: const MarkerId("HamburgMarker"),
+                      rotation: 135,
+                      infoWindow: const InfoWindow(
+                          title: "Hamburg",
+                          snippet:
+                              "Location is 53.556060743806164, 9.96687243215407"),
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueMagenta),
+                      position:
+                          const LatLng(53.556060743806164, 9.96687243215407)),
+                  (_customIcon == null)
+                      ? const Marker(
+                          markerId: MarkerId("MünchenMarker"),
+                          rotation: 315,
+                          infoWindow: InfoWindow(
+                              title: "München",
+                              snippet:
+                                  "Location is 48.1272193947633, 11.555870900278869"),
+                          position:
+                              LatLng(48.1272193947633, 11.555870900278869))
+                      : Marker(
+                          markerId: const MarkerId("MünchenMarker"),
+                          rotation: 315,
+                          infoWindow: const InfoWindow(
+                              title: "München",
+                              snippet:
+                                  "Location is 48.1272193947633, 11.555870900278869"),
+                          icon: _customIcon!,
+                          position: const LatLng(
+                              48.1272193947633, 11.555870900278869))
+                },
+                circles: <Circle>{
+                  Circle(
+                      circleId: const CircleId("BerlinCircle"),
+                      consumeTapEvents: true,
+                      onTap: () => print("onTap: Berlin circle tapped"),
+                      center:
+                          const LatLng(52.52309894124325, 13.413122125924026),
+                      radius: 25000,
+                      fillColor: Colors.grey.shade300,
+                      strokeWidth: 1,
+                      strokeColor: Colors.black),
+                  Circle(
+                      circleId: const CircleId("StuttgartCircle"),
+                      consumeTapEvents: true,
+                      onTap: () => print("onTap: Stuttgart circle tapped"),
+                      center: const LatLng(48.77538798756846, 9.18178996130423),
+                      radius: 25000,
+                      fillColor: Colors.grey.shade300,
+                      strokeWidth: 1,
+                      strokeColor: Colors.black),
+                  Circle(
+                      circleId: const CircleId("FrankfurtCircle"),
+                      consumeTapEvents: true,
+                      onTap: () => print("onTap: Frankfurt circle tapped"),
+                      center:
+                          const LatLng(50.11655301417056, 8.681184968800679),
+                      radius: 25000,
+                      fillColor: Colors.grey.shade300,
+                      strokeWidth: 1,
+                      strokeColor: Colors.black),
+                  Circle(
+                      circleId: const CircleId("DüsseldorfCircle"),
+                      consumeTapEvents: true,
+                      onTap: () => print("onTap: Düsseldorf circle tapped"),
+                      center:
+                          const LatLng(51.22837532659796, 6.788875495904476),
+                      radius: 25000,
+                      fillColor: Colors.grey.shade300,
+                      strokeWidth: 1,
+                      strokeColor: Colors.black),
+                  Circle(
+                      circleId: const CircleId("HamburgCircle"),
+                      consumeTapEvents: true,
+                      onTap: () => print("onTap: Hamburg circle tapped"),
+                      center:
+                          const LatLng(53.556060743806164, 9.96687243215407),
+                      radius: 25000,
+                      fillColor: Colors.grey.shade300,
+                      strokeWidth: 1,
+                      strokeColor: Colors.black),
+                  Circle(
+                      circleId: const CircleId("MünchenCircle"),
+                      consumeTapEvents: true,
+                      onTap: () => print("onTap: München circle tapped"),
+                      center:
+                          const LatLng(48.1272193947633, 11.555870900278869),
+                      radius: 25000,
+                      fillColor: Colors.grey.shade300,
+                      strokeWidth: 1,
+                      strokeColor: Colors.black)
+                },
+                initialCameraPosition: const CameraPosition(
+                    zoom: 15,
+                    target: LatLng(52.52309894124325, 13.413122125924026)))));
   }
 }
